ba108e6feb74511e2b194dbe8dd537939c95b664
sharathdoes <justabountyhunter935@gmail.com>
2026-02-12 01:55:22 +0530

    lru-mutex-locks-added

--- lru-cache/internal/dll/dll.go
+++ lru-cache/internal/dll/dll.go
@@ -1,60 +1,57 @@
 package dll
 
-type DLL struct {
-	Head *Node
-	Tail *Node
-	capacity int
-}
+import "sync"
 
+type DLL struct {
+	Head     *Node
+	Tail     *Node
+	capacity int
+	mu       sync.RWMutex
+}
 
 func NewDLL(cap int) *DLL {
-    head := &Node{}
-    tail := &Node{}
+	head := &Node{}
+	tail := &Node{}
 
-    head.Next = tail
-    tail.Prev = head
+	head.Next = tail
+	tail.Prev = head
 
-    return &DLL{
-        Head: head,
-        Tail: tail,
-        capacity: cap,
-    }
+	return &DLL{
+		Head:     head,
+		Tail:     tail,
+		capacity: cap,
+	}
 }
-
 
 func (d *DLL) AddFirst(new_node *Node) {
-	first:=d.Head.Next
-	d.Head.Next=new_node
-	new_node.Next=first
-	new_node.Prev=d.Head
-	first.Prev=new_node
+	first := d.Head.Next
+	d.Head.Next = new_node
+	new_node.Next = first
+	new_node.Prev = d.Head
+	first.Prev = new_node
 }
 
-func(d *DLL)RemoveNode(n *Node){
-	prev:=n.Prev
-	next:=n.Next
-	prev.Next=next
-	next.Prev=prev
+func (d *DLL) RemoveNode(n *Node) {
+	prev := n.Prev
+	next := n.Next
+	prev.Next = next
+	next.Prev = prev
 }
-func(d *DLL)RemoveBeginning() *Node {
+func (d *DLL) RemoveBeginning() *Node {
 	if d.Head.Next == d.Tail {
 		return nil
 	}
-	node:=d.Head.Next
-	next_node:=node.Next
-	d.Head.Next=next_node
-	next_node.Prev=d.Head
+	node := d.Head.Next
+	next_node := node.Next
+	d.Head.Next = next_node
+	next_node.Prev = d.Head
 	return node
 }
 
-func (d *DLL) InsertAtLast(new_node *Node){
-	last:=d.Tail.Prev
-	last.Next=new_node
-	d.Tail.Prev=new_node
-	new_node.Next=d.Tail
-	new_node.Prev=last
+func (d *DLL) InsertAtLast(new_node *Node) {
+	last := d.Tail.Prev
+	last.Next = new_node
+	d.Tail.Prev = new_node
+	new_node.Next = d.Tail
+	new_node.Prev = last
 }
-
-
-
-
--- lru-cache/internal/dll/lru.go
+++ lru-cache/internal/dll/lru.go
@@ -14,7 +14,11 @@ func NewLRU(cap int) *LRU {
 
 
 func(l *LRU)PushValue(key string, value string){
-		if node, ok := l.Mp[key]; ok {
+
+	l.Dll.mu.Lock()
+	defer l.Dll.mu.Unlock()
+	
+	if node, ok := l.Mp[key]; ok {
 		l.Dll.RemoveNode(node)
 		node.Value=value
 		l.Dll.InsertAtLast(node)
@@ -32,6 +36,8 @@ func(l *LRU)PushValue(key string, value string){
 }
 
 func (l *LRU) GetValue(key string) (string, bool) {
+	l.Dll.mu.Lock()
+	defer l.Dll.mu.Unlock()
     node, ok := l.Mp[key]
     if !ok {
         return "", false
